0465fe1d4f823dad98dbe4b11376f251b70cd308
tnhuhoang1 <1851061587@e.tlu.edu.vn>
2022-02-11 19:22:17 +0700

    add dependencies and change app icon

--- app/src/main/java/com/tnh/mollert/MainActivity.kt
+++ app/src/main/java/com/tnh/mollert/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.tnh.mollert
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.activity_main_fragment_container) as NavHostFragment).navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/tnh/mollert/TestFragment.kt
@@ -0,0 +1,20 @@
+package com.tnh.mollert
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class TestFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return TextView(requireContext()).apply {
+            text = "nhu hoang morllert"
+        }
+    }
+}
