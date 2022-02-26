0ce41dba66a2d3666788db2de43fbf105ecd5c88
linh16p <1851060148@e.tlu.edu.vn>
2022-02-26 10:23:46 +0700

    edit file backgroundPreset

--- app/src/main/java/com/tnh/mollert/utils/BackgroundPreset.kt
+++ app/src/main/java/com/tnh/mollert/utils/BackgroundPreset.kt
@@ -10,6 +10,18 @@ object BackgroundPreset {
         "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2Fphoto-1645611041935-d3450e58a64a.jpg?alt=media&token=03ad4f88-1cd8-492c-89b2-38a0cda420ef",
         "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2Fphoto-1645611868479-c5aa51b8e77f.jpg?alt=media&token=1e89af01-6ebe-4653-af9f-c1a7369e6a1b",
         "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2Fphoto-1645645271595-c40a86fd1cb4.jpg?alt=media&token=df2bae06-2832-4afd-b0ef-0bfb72360da3",
-        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2Fphoto-1645747103867-0669a7eff310.jpg?alt=media&token=b3a5f745-4e40-45c6-a0c0-f3c51f7d6fae"
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2Fphoto-1645747103867-0669a7eff310.jpg?alt=media&token=b3a5f745-4e40-45c6-a0c0-f3c51f7d6fae",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F1.jpg?alt=media&token=df7c1380-172a-4572-95ad-65b1aa30431a",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F2.jpg?alt=media&token=8814a1f6-c3f3-4859-bdcf-8f19e5f6d105",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F3.jpg?alt=media&token=123e0e16-00f0-481c-9c97-f934ca67da5b",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F4.jpg?alt=media&token=9c9af307-8f94-4a07-be64-c81823ec24a2",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F5.jpg?alt=media&token=5207b7d0-4794-431b-8c7c-f16064654100",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F6.jpg?alt=media&token=8ba04f5b-8ce3-4a87-b768-7100595765ef",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F7.jpg?alt=media&token=1bcb0628-50a4-479d-bc13-e2e41df37aef",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F8.jpg?alt=media&token=ea4dfdbd-62a3-434f-b73a-24c29f18e7b0",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F9.jpg?alt=media&token=7539c860-f079-434b-b189-d25fd8ca76b7",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F10.jpg?alt=media&token=04713974-9f30-427f-8187-6cfe37d497bb",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F11.jpg?alt=media&token=440deb0c-a33b-49cc-852a-d74ee9b581cc",
+        "https://firebasestorage.googleapis.com/v0/b/mollert-fb39e.appspot.com/o/default%2F12.jpg?alt=media&token=0ce4938e-880a-4305-898e-4829d32c8846"
     )
 }
